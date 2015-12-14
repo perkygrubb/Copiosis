44c219846b58085ae0a068c52c164c0e83c759fc
ExtraordinaryBen <ben@powerpup.yi.org>
2015-12-13 21:16:34 -0800

    Override SaveChanges in DB_Data
    Override the SaveChanges method in DB_Data so that cleaner error messages are presented.

--- Copiosis Application/DB_Data/CopiosisDBModel.Context.cs
+++ Copiosis Application/DB_Data/CopiosisDBModel.Context.cs
@@ -32,4 +32,31 @@ namespace Copiosis_Application.DB_Data
         public virtual DbSet<user> users { get; set; }
         public virtual DbSet<webpages_Roles> webpages_Roles { get; set; }
     }
+    
+    
+    /*  Credit to StackOverFlow user Martin Devillers for providing a method to output
+        cleaner error messages: http://stackoverflow.com/a/15820506 */
+        public override int SaveChanges()
+    {
+        try
+        {
+            return base.SaveChanges();
+        }
+        catch (DbEntityValidationException ex)
+        {
+            // Retrieve the error messages as a list of strings.
+            var errorMessages = ex.EntityValidationErrors
+                    .SelectMany(x => x.ValidationErrors)
+                    .Select(x => x.ErrorMessage);
+    
+            // Join the list to a single string.
+            var fullErrorMessage = string.Join("; ", errorMessages);
+    
+            // Combine the original exception message with the new one.
+            var exceptionMessage = string.Concat(ex.Message, " The validation errors are: ", fullErrorMessage);
+    
+            // Throw a new DbEntityValidationException with the improved exception message.
+            throw new DbEntityValidationException(exceptionMessage, ex.EntityValidationErrors);
+        }
+    }
 }
